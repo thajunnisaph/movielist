23eb8f5f8183e8f6e0656b819ba455f0f7c9892f
thajunnisaph <thajunnisaph123@gmail.com>
2022-12-07 15:19:00 +0530

    add a loader till data fetch from backend

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
    const response = await fetch("https://swapi.dev/api/films/");
+
        const data= await response.json();
       
   
@@ -19,6 +22,7 @@ function App() {
           };
         });
         setMovies(transformMovies);
+        setIsLoading(false);
       
   }
 
@@ -28,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        { !isLoading && movies.length>0 && <MoviesList movies={movies} />}
+        { !isLoading && movies.length ===0 && <p>No Movies Found</p>}
+        {isLoading && <p> loading ...</p>}
       </section>
     </React.Fragment>
   );
